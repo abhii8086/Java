246b72843c4a6d6d2a2aca8ebcff662c5a7b1335
Abhijeet R Sharma <97837777+abhii8086@users.noreply.github.com>
2023-06-02 22:00:22 +0530

    Update Analaysis_of_complexites.java

--- Time_and_Space_Complexity/Analaysis_of_complexites.java
+++ Time_and_Space_Complexity/Analaysis_of_complexites.java
@@ -3,8 +3,26 @@ package Time_and_Space_Complexity;
 public class Analaysis_of_complexites {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+        for(int i = 0;i<=n; i++) // O(n) liner
+        	
+        	for(int i=0;i<=n;i++) {        //O(n*n); square
+        		for(int j = 0 j<m;j++) {
+        			
+        		}
+        	}
+        
+        if(i>3) {           // O(1) constant
+        	
+        }else {
+        	
+        }
+           
+        for(int i=0 i<5;i++) { // O(1) constant
+        	
+        }
+        	
 	}
 
 }
+
